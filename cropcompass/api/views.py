7a44e457917b27b24153087a23837fe79748d479
atifar <tyatya68@gmail.com>
2016-04-17 23:50:36 -0700

    Add documentation to views

--- cropcompass/api/views.py
+++ cropcompass/api/views.py
@@ -1,3 +1,21 @@
+"""
+API views.
+
+The API serves the response in either browsable format, by default, or when
+including a "format=api" query parameter, or in JSON format, when including
+a "format=json" query parameter.
+
+The list views that support optional filtering indicate that in their
+docstrings. For those views filtering can be applied as follows.
+
+A URL may optionally include query parameters to filter the
+JSON response. Query parameter keys that cause filtering should be
+one of the field names in the FILTER_FIELDS list. Any other key is
+ignored. The corresponding value is exact matched against the field
+value in every table row. Multiple "key=value" pairs may be provided,
+in any order, including multiple values for the same key.
+"""
+
 from django.contrib.auth.models import User, Group
 from django.http import Http404
 from rest_framework import viewsets, status
@@ -41,17 +59,25 @@ class NassAnimalsSalesList(APIView):
     List animal sales or create new instance.
     """
     def get(self, request, format=None):
-        """List animal sales with optional filtering on year or commodity."""
+        """List animal sales with optional filtering from nass_animals_sales.
+
+        Example 1: GET /nass_animals_sales/
+        Returns all rows of the table in browsable API format, which is the default format, also specifiable by the query parameter "format=api".
+
+        Example 2:
+        GET /nass_animals_sales/?year=1997&commodity=Bison&year=2002&format=json
+        Returns rows of Bison from 1997 and 2002 in JSON format.
+        """
         # Accepted fields for filtering output
-        FILTERS = ['commodity', 'year']
+        FILTER_FIELDS = ['commodity', 'year']
         if request.query_params:
             # Build dictionary of filter parameters (exclude 'format', etc.)
             filter_params = {}
             for param in request.query_params.keys():
-                vals = request.query_params.getlist(param)
-                if param in FILTERS:
+                if param in FILTER_FIELDS:
+                    vals = request.query_params.getlist(param)
                     filter_params[param] = vals
-            # Augment dictionary keys with __in for OR-ing values in the query
+            # Augment dictionary keys with __in for looking up list inclusion
             query = {key + '__in': vals for key, vals in filter_params.items()}
             # Generate queryset
             animal_sales = NassAnimalsSales.objects.filter(**query)
@@ -72,7 +98,12 @@ class NassAnimalsSalesDetail(APIView):
             raise Http404
 
     def get(self, request, pk, format=None):
-        """Retrieve animal sales instance."""
+        """Retrieve animal sales instance from nass_animals_sales.
+
+        Example 1:
+        GET /nass_animals_sales/2803/
+        Returns the item with the id of 2803.
+        """
         animal_sales = self.get_object(pk)
         serializer = NassAnimalsSalesSerializer(animal_sales)
         return Response(serializer.data)
